4f9907390f98dc6ac81b00ff25f3f7ddc469bf1a
storgall <32178784+storgall@users.noreply.github.com>
2017-09-21 17:35:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,14 @@
 	
 	
 	API.getWaitListPosition = function(id) {
+		API.sendChat("1")
 		if (typeof id === 'undefined' || id === null) {
+			API.sendChat("2")
 			id = API.getUser().id;
 		}
 		var wl = API.getWaitList();
 		for (var i = 0; i < wl.length; i++) {
+			API.sendChat("3")
 			if (wl[i].id === id) {
 				return i;
 			}
