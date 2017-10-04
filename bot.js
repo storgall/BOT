90842681b2bcab157a9caacb7105050919c05377
storgall <32178784+storgall@users.noreply.github.com>
2017-10-04 17:22:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,35 @@
 (function(){	
 	
-	API.getWaitListPosition = function(id) {
-		if (typeof id === 'undefined' || id === null) {
-			id = API.getUser().id;
-		}
-		var wl = API.getWaitList();
-		for (var i = 0; i < wl.length; i++) {
-			if (wl[i].id === id) {
-				return i;
-			}
-		}
-		return -1;
-	};
+//	API.getWaitListPosition = function(id) {
+//		if (typeof id === 'undefined' || id === null) {
+//			id = API.getUser().id;
+//		}
+//		var wl = API.getWaitList();
+//		for (var i = 0; i < wl.length; i++) {
+//			if (wl[i].id === id) {
+//				return i;
+//			}
+//		}
+//		return -1;
+//	};
 	
 	
 	
  	var init = function(){
-		API.sendChat(API.getWaitListPosition(6117568).toString())
-		API.sendChat(stor["version"].toString())
-		API.sendChat(API.getUser(6117568)["id"].toString())
-		API.sendChat("Done!")
+//		API.sendChat(API.getWaitListPosition(6117568).toString())
+//		API.sendChat(stor["version"].toString())
+//		API.sendChat(API.getUser(6117568)["id"].toString())
+//		API.sendChat("Done!")
 		for(var i=0, i<1000, i++}{
 		    API.sendChat("@-Máro- ");
 		}
 	}
 
-	var stor = {
-		"version": "0.0",
-		"status": false,
-		"name": "stor"
-	}
+//	var stor = {
+//		"version": "0.0",
+//		"status": false,
+//		"name": "stor"
+//	}
 	
 	
 	
