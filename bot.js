babebceb215742869464d6b061bc63b16fa735ab
storgall <32178784+storgall@users.noreply.github.com>
2018-02-03 13:51:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,117 @@
-(function(){	
-	
-//	API.getWaitListPosition = function(id) {
-//		if (typeof id === 'undefined' || id === null) {
-//			id = API.getUser().id;
-//		}
-//		var wl = API.getWaitList();
-//		for (var i = 0; i < wl.length; i++) {
-//			if (wl[i].id === id) {
-//				return i;
-//			}
-//		}
-//		return -1;
-//	};
-	
-	
-	
- 	var init = function(){
-//		API.sendChat(API.getWaitListPosition(6117568).toString())
-//		API.sendChat(stor["version"].toString())
-//		API.sendChat(API.getUser(6117568)["id"].toString())
-//		API.sendChat("Done!")
-		for(var i=0, i<1000, i++}{
-		    API.sendChat("@-Máro- ");
-		}
-	}
+(function () {
 
-//	var stor = {
-//		"version": "0.0",
-//		"status": false,
-//		"name": "stor"
-//	}
-	
-	
-	
-	init();
+    // Change this to your GitHub username so you don't have to modify so many things.
+    var fork = "Freyr";
+
+    // Define our function responsible for extending the bot.
+    function extend() {
+        // If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+          return setTimeout(extend, 1 * 1000);
+        }
+
+        // Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        // Load custom settings set below
+        bot.retrieveSettings();
+
+        //Extend the bot here, either by calling another function or here directly.
+
+        // You can add more spam words to the bot.
+        var spamWords = ['spam1', 'spam2', 'spam3', 'spam4'];
+        for (var i = 0; i < spamWords.length; i++) {
+          window.bot.chatUtilities.spam.push(spamWords[i]);
+        }
+
+        // Example code for a bot command:
+        bot.commands.superCommand = {
+          command: 'super',  // The command to be called. With the standard command literal this would be: !bacon
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("/me Nejlepší hudba 11/10!!!");
+            }
+          }
+        };
+
+        // Load the chat package again to account for any changes
+        bot.loadChat();
+
+      }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    localStorage.setItem("basicBotsettings", JSON.stringify({
+      botName: "MagicalBOT",
+      language: "czech",
+      chatLink: "https://rawgit.com/basicBot/source/master/lang/cs.json",
+      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      roomLock: false, // Requires an extension to re-load the script
+      startupCap: 69, // 1-200
+      startupVolume: 100, // 0-100
+      startupEmoji: true, // true or false
+      autowoot: true,
+      autoskip: true,
+      smartSkip: true,
+      cmdDeletion: true,
+      maximumAfk: 120,
+      afkRemoval: true,
+      maximumDc: 60,
+      bouncerPlus: true,
+      blacklistEnabled: true,
+      lockdownEnabled: false,
+      lockGuard: false,
+      maximumLocktime: 10,
+      cycleGuard: true,
+      maximumCycletime: 10,
+      voteSkip: true,
+      voteSkipLimit: 8,
+      historySkip: true,
+      timeGuard: true,
+      maximumSongLength: 6.30,
+      autodisable: true,
+      commandCooldown: 30,
+      usercommandsEnabled: true,
+      skipPosition: 3,
+      skipReasons: [
+      ["theme", "This song does not fit the room theme. "],
+      ["op", "This song is on the OP list. "],
+      ["history", "This song is in the history. "],
+      ["mix", "You played a mix, which is against the rules. "],
+      ["sound", "The song you played had bad sound quality or no sound. "],
+      ["nsfw", "The song you contained was NSFW (image or sound). "],
+      ["unavailable", "The song you played was not available for some users. "]
+      ],
+      afkpositionCheck: 15,
+      afkRankCheck: "ambassador",
+      motdEnabled: false,
+      motdInterval: 5,
+      motd: "Temporary Message of the Day",
+      filterChat: false,
+      etaRestriction: false,
+      welcome: false,
+      opLink: null,
+      rulesLink: "https://i.imgur.com/yOZd3aq.png",
+      themeLink: null,
+      fbLink: null,
+      youtubeLink: null,
+      website: null,
+      intervalMessages: ["Iluška Vás všechny miluje <3"],
+      messageInterval: 20,
+      songstats: false,
+      commandLiteral: "!",
+      blacklists: {
+        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+      }
+    }));
+
+    // Start the bot and extend it when it has loaded.
+    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
 
 }).call(this);
