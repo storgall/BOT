76927c8109cdcdb8640dae89b64ffae9e0832376
storgall <32178784+storgall@users.noreply.github.com>
2018-03-26 20:38:19 -0400

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -403,11 +403,11 @@
                             basicBot.room.roulette.participants.splice(koko,1);
                         }
                     }
-                    /*var array_id = "";
+                    /*var arrayid = "";
                     for (var koki = 0; koki < basicBot.room.roulette.participants.length; koki++){
-                        var array_id += basicBot.room.roulette.participants[koki].toString()+", ";
+                        var arrayid += basicBot.room.roulette.participants[koki].toString()+", ";
                     }
-                    API.sendChat(array_id);*/
+                    API.sendChat(arrayid);*/
                     API.sendChat(idecka);
                     var ind = Math.floor(Math.random() * basicBot.room.roulette.participants.length);
                     var winner = basicBot.room.roulette.participants[ind];
